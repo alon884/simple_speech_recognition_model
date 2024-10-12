6c08c87808fb5b04290972d50acdfd466bb0a1ad
alon884 <alon60733@gmail.com>
2024-10-12 15:47:45 +0300

    high level code done. and now want to push to model-branch

---
+++ train.py
@@ -0,0 +1,32 @@
+
+
+
+
+
+DATA_PATH = "/media/alon/DATA/ProjectsForCV/proj1/dataset"
+SAVED_MODEL_PATH = "/media/alon/DATA/ProjectsForCV/proj1/model.h5"
+
+LEARNING_RATE = 0.0001
+EPOCHS = 40
+BATCH_SIZE = 32
+
+def main():
+
+	# load train/validation/test data splits
+	X_train, X_validation, X_test, y_train, y_validation, y_test = get_data_splits(DATA_PATH)
+
+
+	# build the CNN model
+	input_shape = (X_train.shape[1], X_train.shape[2], X_train.shape[3]) # (# segments, # coefficients 13, 1)
+
+	model = build_model(input_shape, LEARNING_RATE)
+
+	# train the model
+	model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, validation_data=(X_validation, y_validation))
+
+	# evaluate the model
+	test_error, test_accuracy = model.evaluate(X_test, y_test)
+	print(f"Test error: {test_error}, test_accuracy: {test_accuracy}")
+
+	# save the model
+	model.save(SAVED_MODEL_PATH)
